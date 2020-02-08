a870ca7d3709c7f90390d1aef64b4f4137bd7c6e
Ilia Zadornov <il.mozma@gmail.com>
2020-02-08 18:46:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace c_sharp_DeskLibrary
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
